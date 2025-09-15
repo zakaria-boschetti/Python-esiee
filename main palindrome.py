39932ebc70b48245371153b0cca6f539e1967d7c
zakaria-boschetti <zakaria.boschetti@edu.esiee.fr>
2025-09-15 15:04:09 +0200

    ceci est un test

---
+++ main palindrome.py
@@ -0,0 +1,24 @@
+
+
+import numpy
+import math
+
+def is_palindrome(n):
+    s = str(n)
+    return s == s[::-1]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ primes.py
@@ -0,0 +1,19 @@
+import math
+import numpy
+
+def premier(n):
+
+    a,b= 0,0
+
+    for i in range (int(n**(1/2))):
+        a+=1
+        print(a)
+        if (n%a)==0 and a !=1 :
+            print(n,"n'est pas premier")
+            break
+        else:
+            print(a,n)
+            print(n,"est  premier")
+
+
+
